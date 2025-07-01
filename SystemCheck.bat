ffba0d4a1fadd8741069d9d3c60b85ff49184bc5
Janusz Kornath <190484075+JanuszKornath@users.noreply.github.com>
2025-07-01 12:45:42 +0200

    Codepage an den Anfang setzen
    Für Umlaute auf die Windows-ANSI-Codepage (Westeuropa, Windows-1252) umstellen

--- SystemCheck.bat
+++ SystemCheck.bat
@@ -1,28 +1,29 @@
 @echo off
+chcp 1252 >nul
 title Systemüberprüfung mit DISM und SFC
 color 1F
 
 echo =============================
-echo   Windows Systemprüfung
+echo   Windows Systempruefung
 echo =============================
 echo.
 
-echo [1/4] DISM: CheckHealth wird ausgeführt...
+echo [1/4] DISM: CheckHealth wird ausgefuehrt...
 Dism /Online /Cleanup-Image /CheckHealth
 echo.
 pause
 
-echo [2/4] DISM: ScanHealth wird ausgeführt...
+echo [2/4] DISM: ScanHealth wird ausgefuehrt...
 Dism /Online /Cleanup-Image /ScanHealth
 echo.
 pause
 
-echo [3/4] DISM: RestoreHealth wird ausgeführt...
+echo [3/4] DISM: RestoreHealth wird ausgefuehrt...
 Dism /Online /Cleanup-Image /RestoreHealth
 echo.
 pause
 
-echo [4/4] SFC: Systemdatei-Überprüfung wird ausgeführt...
+echo [4/4] SFC: Systemdatei-Ueberpruefung wird ausgefuehrt...
 sfc /scannow
 echo.
 pause
