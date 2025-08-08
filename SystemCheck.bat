8abafd4c12b227f9eb698451002593b321897498
Janusz Kornath <190484075+JanuszKornath@users.noreply.github.com>
2025-08-08 07:43:46 +0200

    Update SystemCheck.bat
    Umlaute

--- SystemCheck.bat
+++ SystemCheck.bat
@@ -1,20 +1,22 @@
-@echo off
+﻿@echo off
+chcp 65001 >nul
 pushd %~dp0
-chcp 1252 >nul
 title Systemüberprüfung mit DISM und SFC
 color 1F
 
 :: Admin-Rechte prüfen (zuverlässig)
 fltmc >nul 2>&1
 if %errorlevel% neq 0 (
-    echo [FEHLER] Bitte als Administrator ausfuehren!
+    color 0C
+    echo [FEHLER] Bitte als Administrator ausführen!
+    color 1F
     pause
     exit /b
 )
 
 :: Logdatei initialisieren
 set LOG=SystemCheck.log
-echo === Systemcheck gestartet: %DATE% %TIME% === > %LOG%
+echo === Systemcheck gestartet: %DATE% %TIME% === > "%LOG%"
 
 echo =============================
 echo   Windows Systemprüfung
@@ -22,9 +24,9 @@ echo =============================
 echo.
 
 echo [1/4] DISM: CheckHealth wird ausgeführt...
-Dism /Online /Cleanup-Image /CheckHealth >> %LOG% 2>&1
+Dism /Online /Cleanup-Image /CheckHealth >> "%LOG%" 2>&1
 if %errorlevel% neq 0 (
-    echo Fehler bei CheckHealth. Details siehe %LOG%.
+    echo Fehler bei CheckHealth. Details siehe "%LOG%".
     pause
     exit /b
 )
@@ -32,9 +34,9 @@ echo.
 pause
 
 echo [2/4] DISM: ScanHealth wird ausgeführt...
-Dism /Online /Cleanup-Image /ScanHealth >> %LOG% 2>&1
+Dism /Online /Cleanup-Image /ScanHealth >> "%LOG%" 2>&1
 if %errorlevel% neq 0 (
-    echo Fehler bei ScanHealth. Details siehe %LOG%.
+    echo Fehler bei ScanHealth. Details siehe "%LOG%".
     pause
     exit /b
 )
@@ -42,9 +44,9 @@ echo.
 pause
 
 echo [3/4] DISM: RestoreHealth wird ausgeführt...
-Dism /Online /Cleanup-Image /RestoreHealth >> %LOG% 2>&1
+Dism /Online /Cleanup-Image /RestoreHealth >> "%LOG%" 2>&1
 if %errorlevel% neq 0 (
-    echo Fehler bei RestoreHealth. Details siehe %LOG%.
+    echo Fehler bei RestoreHealth. Details siehe "%LOG%".
     pause
     exit /b
 )
@@ -52,9 +54,9 @@ echo.
 pause
 
 echo [4/4] SFC: Systemdatei-Überprüfung wird ausgeführt...
-sfc /scannow >> %LOG% 2>&1
+sfc /scannow >> "%LOG%" 2>&1
 if %errorlevel% neq 0 (
-    echo Fehler bei SFC. Details siehe %LOG%.
+    echo Fehler bei SFC. Details siehe "%LOG%".
     pause
     exit /b
 )
@@ -64,6 +66,9 @@ pause
 echo =============================
 echo     Vorgang abgeschlossen
 echo =============================
-echo Siehe Logdatei: %LOG%
+echo Siehe Logdatei: "%LOG%"
+echo.
+echo Letzte Einträge aus der Logdatei:
+type "%LOG%" | more
 pause
 exit
